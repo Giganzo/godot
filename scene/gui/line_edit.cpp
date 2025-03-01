39194fa4723656b1a4f2c072c0a49c53fe026eac
Giganzo <158825920+Giganzo@users.noreply.github.com>
2025-03-01 16:22:43 +0100

    Fix LineEdit caret_force_displayed not displaying caret when true

--- scene/gui/line_edit.cpp
+++ scene/gui/line_edit.cpp
@@ -1733,7 +1733,7 @@ void LineEdit::_validate_caret_can_draw() {
 		draw_caret = true;
 		caret_blink_timer = 0.0;
 	}
-	caret_can_draw = editing && (window_has_focus || (menu && menu->has_focus())) && (has_focus() || caret_force_displayed);
+	caret_can_draw = editable && (window_has_focus || (menu && menu->has_focus())) && (has_focus() || caret_force_displayed);
 }
 
 void LineEdit::delete_char() {
